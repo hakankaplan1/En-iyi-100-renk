da0f869f5fc9979950f2f6bae004fc436393ba95
hakan <hknkap99@gmail.com>
2024-02-19 14:07:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import "./styles.css";
+
+import colorData from "./data/colorData";
+import Header from "./components/Header";
+
+export default function App() {
+  /* Challenge
+
+	Frontend Dergisi, okuyucularına front-end geliştirmedeki en iyi 100 rengi göstermenizi bekliyor. Göreviniz onları aşağıdaki gibi oluşturmak: 
+	
+		1. colorData dizisindeki 100 hex kodlu rengin her biri için aşağıda işaretli alanda bir ColorCard elementi bulunmalıdır. Renkler zaten sıralanmıştır, bu nedenle elementlerin sırası array'deki renklerin sırası ile eşleşmelidir. 
+		   
+		2. colorData bileşenindeki sabit kodlu bilgiler, prop'lar aracılığıyla aktarılan verilerle değiştirilmelidir. 
+		   
+		3. Bu görevleri doğru bir şekilde tamamlarsanız, "En İyi 100 Renk" başlığının altında 100 renk kartı görmelisiniz. Her renk, ilgili rengin sıra numarasını, hex kodunu ve gerçek rengin kendisini arka planda göstermelidir
+		   
+		Not: colorData dosyaya aktarılmıştır
+*/
+
+  function ColorCard({ index, colorCardCode }) {
+    return (
+      <div className="color-card" style={{ background: `${colorCardCode}` }}>
+        <p>
+          <span>{index + 1}.</span> {colorCardCode}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <Header />
+
+      {/*------ColorCard elementleriniz aşağıda-----------*/}
+
+      {colorData.map((color, index) => {
+        return (
+          <ColorCard
+            key={crypto.randomUUID()}
+            index={index}
+            colorCardCode={color}
+          />
+        );
+      })}
+
+      {/*------ ColorCard elementleriniz yukarıda-----------*/}
+    </div>
+  );
+}
